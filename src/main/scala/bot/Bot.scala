d259487efcf17e919b389de1207ca418922636aa
Rob Haley <rob_haley@live.co.uk>
2015-08-13 22:24:21 +0100

    add case for solids

--- src/main/scala/bot/Bot.scala
+++ src/main/scala/bot/Bot.scala
@@ -10,7 +10,8 @@ class Bot {
     }
 
     def getPossibleEndLocations(field: Field, shapeType: ShapeType, shapeLocation: Location): List[Location] = {
-        val asLowAsPossible = if(field.getCell(0, field.height-1).state == BLOCK) field.height - 3 else field.height -2
+        val bottomLeftState = field.getCell(0, field.height-1).state;
+        val asLowAsPossible = if(bottomLeftState == BLOCK || bottomLeftState == SOLID) field.height - 3 else field.height -2
         (0 to field.width - 2).map(x => Location(x, asLowAsPossible)).toList
     }
 }
