df7c63f997995ac8b7c2bf5dd0d2336cdffca539
Rob Haley <rob_haley@live.co.uk>
2015-08-21 12:51:35 +0100

    add support for possible end states of J shape

--- src/main/scala/bot/Bot.scala
+++ src/main/scala/bot/Bot.scala
@@ -12,8 +12,15 @@ class Bot {
     def getPossibleEndStates(field: Field, shapeType: ShapeType): Seq[Field] = {
         shapeType match {
             case O =>   Shape(O, 0).allPossibleEndLocations(field).map(loc => field.withShapeAt(Shape(O, 0), loc))
+
             case I =>   Shape(I, 0).allPossibleEndLocations(field).map(loc => field.withShapeAt(Shape(I, 0), loc)) ++
                         Shape(I, 90).allPossibleEndLocations(field).map(loc => field.withShapeAt(Shape(I, 90), loc))
+
+            case J =>   Shape(J, 0).allPossibleEndLocations(field).map(loc => field.withShapeAt(Shape(J, 0), loc)) ++
+                        Shape(J, 90).allPossibleEndLocations(field).map(loc => field.withShapeAt(Shape(J, 90), loc)) ++
+                        Shape(J, 180).allPossibleEndLocations(field).map(loc => field.withShapeAt(Shape(J, 180), loc)) ++
+                        Shape(J, 270).allPossibleEndLocations(field).map(loc => field.withShapeAt(Shape(J, 270), loc))
+
         }
     }
 }
