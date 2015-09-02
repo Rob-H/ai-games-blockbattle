9efd782e4293d032389228677176b9607139b761
Rob Haley <rob_haley@live.co.uk>
2015-09-02 13:40:25 +0100

    add initial very simple get moves
    * currently only takes one block at a time in to account

--- src/main/scala/bot/Bot.scala
+++ src/main/scala/bot/Bot.scala
@@ -4,8 +4,17 @@ import field._
 import moves._
 
 class Bot {
-    def getMoves(state: BotState, timeout: Long): List[MoveType] = {
-        List()
+    def getMoves(state: BotState, timeout: Long): Seq[MoveType] = {
+        val startState = new FieldInPlay(state.getMyField, Shape(state.getCurrentShape), state.getShapeLocation)
+        val possibleEndStates = (getPossibleEndStates(state.getMyField, state.getCurrentShape) sortBy (-_.heuristic)).toStream
+
+        (for {
+            nextIdealEndState <- possibleEndStates
+            path <- getPathTo(startState, nextIdealEndState)
+        } yield path) match {
+            case head #:: _ => head
+            case _ => Nil
+        }
     }
 
     def getPossibleEndStates(field: Field, shapeType: ShapeType): Seq[Field] = {
