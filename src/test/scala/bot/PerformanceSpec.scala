2f4bb225a2f3bdb4bbc12f70c48fd5821bf77d7c
Rob Haley <rob_haley@live.co.uk>
2015-09-15 13:01:11 +0100

    add perf test, also add repeat runs and warming

--- src/test/scala/bot/PerformanceSpec.scala
+++ src/test/scala/bot/PerformanceSpec.scala
@@ -1,9 +1,15 @@
-import org.scalameter.measure
+import org.scalameter._
 import org.scalatest._
 import bot._
 import field._
 
 class PerformanceSpec extends FunSpec with Matchers {
+    private val timesToRun = 10
+    private val acceptableMillisecondsToComplete = 400
+
+    private def repeatMeasureWithWarming(block: => Unit) =
+        config(Key.exec.benchRuns -> timesToRun) withWarmer { new Warmer.Default } measure { block }
+
     describe("getting possible end states is performant") {
         it("for this scenario with a J and a T") {
             val field = Field(
@@ -29,12 +35,42 @@ class PerformanceSpec extends FunSpec with Matchers {
                 "2,2,0,2,2,2,2,2,2,2"
             )
 
-            val time = measure {
+            val time = repeatMeasureWithWarming {
                 new Bot().getOrderedPossibleEndStates(field, J, T)
             }
 
-            // scalastyle:off
-            time.value.toInt should be <= 400
+            time.value.toInt should be <= acceptableMillisecondsToComplete
+        }
+
+        it("for this scenario with two S shapes") {
+            val field = Field(
+                "0,0,0,1,1,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,0,0,0,0,0,0,0,0,0;" +
+                "0,2,2,0,0,0,0,0,0,0;" +
+                "2,2,0,0,0,0,0,0,0,0"
+            )
+
+            val time = repeatMeasureWithWarming {
+                new Bot().getOrderedPossibleEndStates(field, S, S)
+            }
+
+            time.value.toInt should be <= acceptableMillisecondsToComplete
         }
     }
 }
